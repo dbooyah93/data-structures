f26c5f1bdce73561fc20885773e19f33d9a5d473
David <davidmcruz1125@gmail.com>
2020-08-19 18:34:26 -0500

    Creating working queue and stack methods under functional

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -1,18 +1,31 @@
 var Queue = function() {
   var someInstance = {};
 
-  // Use an object with numeric keys to store values
+  let counter = 0;
+
   var storage = {};
 
-  // Implement the methods below
-
   someInstance.enqueue = function(value) {
+
+    storage[counter] = value;
+    someInstance[counter] = value;
+    counter++;
+
+
   };
 
   someInstance.dequeue = function() {
+    let deleteKey = (Object.keys(someInstance)[0]);
+    let tmpVar = storage[deleteKey];
+
+    delete storage[deleteKey];
+    delete someInstance[deleteKey];
+
+    return tmpVar;
   };
 
   someInstance.size = function() {
+    return Object.keys(storage).length;
   };
 
   return someInstance;
--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -3,15 +3,27 @@ var Stack = function() {
 
   // Use an object with numeric keys to store values
   var storage = {};
+  let counter = 0;
 
   // Implement the methods below
   someInstance.push = function(value) {
+    storage[counter] = value;
+    someInstance[counter] = value;
+    counter++;
   };
 
   someInstance.pop = function() {
+    let keys = Object.keys(storage); // [array, of, keys]
+    let deleteKey = keys[ keys.length - 1 ];
+    let tempVar = storage[deleteKey];
+
+    delete storage[deleteKey];
+    delete someInstance[deleteKey];
+    return tempVar;
   };
 
   someInstance.size = function() {
+    return Object.keys(storage).length;
   };
 
   return someInstance;
