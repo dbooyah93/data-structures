5b11700e7da322c73895331a8c50f9b6e6c1615a
David <davidmcruz1125@gmail.com>
2020-08-20 12:25:45 -0500

    Rough draft and first attempt at function-shared (queue.js)

--- sprint-one/src/functional-shared/queue.js
+++ sprint-one/src/functional-shared/queue.js
@@ -1,8 +1,45 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+
+  // create an object that holds the methods that will be shared by all instances
+  // use keyword `this`
+  // use _.extend to copy the methods onto the instance
+
+  // attach queueMethods
+  let self = this;
+  _.extend(Queue, queueMethods);
+
 };
 
-var queueMethods = {};
+var queueMethods = {
+
+
+  enqueue: function(value) {
+
+    storage[counter] = value;
+    someInstance[counter] = value;
+    counter++;
+
+
+  },
+
+  dequeue: function() {
+    let deleteKey = (Object.keys(someInstance)[0]);
+    let tmpVar = storage[deleteKey];
+
+    delete storage[deleteKey];
+    delete someInstance[deleteKey];
+
+    return tmpVar;
+  },
+
+  size: function() {
+    return Object.keys(storage).length;
+  }
+
+
+};
+
 
 
