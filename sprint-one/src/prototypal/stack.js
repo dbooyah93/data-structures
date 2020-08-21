f416415fb3a51bbc631de2946f6c0bc59924536f
David <davidmcruz1125@gmail.com>
2020-08-20 19:13:16 -0500

    Implement queue and stack methods for pseudoclassical style

--- sprint-one/src/prototypal/stack.js
+++ sprint-one/src/prototypal/stack.js
@@ -1,8 +1,47 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  let newInstance = Object.create(stackMethods);
+  newInstance.storage = {};
+  newInstance.counter = 0;
+
+  return newInstance;
 };
 
-var stackMethods = {};
+var stackMethods = {
+
+
+  push: function(value) {
+
+    this.storage[this. counter] = value;
+    this[this.counter] = value;
+    this.counter++;
+
+  },
+
+
+
+  pop: function() {
+
+    let keys = Object.keys(this.storage);
+    let popKey = keys[keys.length - 1];
+    let tmpVar = this.storage[popKey];
+    delete this.storage[popKey];
+    delete this[popKey];
+
+
+    return tmpVar;
+
+  },
+
+
+
+  size: function() {
+    return Object.keys(this.storage).length;
+  }
+
+
+
+};
 
 
--- sprint-one/src/pseudoclassical/queue.js
+++ sprint-one/src/pseudoclassical/queue.js
@@ -1,6 +1,56 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  // new instance already made
+
+  this.storage = {};
+  this.counter = 0;
+  // this === Queue
+  // new instance already
+};
+
+Queue.prototype.enqueue = function (value) {
+
+  this.storage[this.counter] = value;
+  this[this.counter] = value;
+  this.counter++;
+
+};
+
+// as we build, the first one we put in === 0 the most recent will be the highest numbers
+
+Queue.prototype.dequeue = function () {
+
+  let deleteKey = Object.keys(this.storage)[0]; // 11
+  let tmpVar = this.storage[deleteKey];
+
+  delete this.storage[deleteKey];
+  delete this[deleteKey];
+
+  return tmpVar;
+};
+
+Queue.prototype.size = function () {
+  console.log(this);
+  return Object.keys(this.storage).length;
+
 };
 
 
+
+
+/**
+ *
+ * only can share methods
+ * auto adds 2 lines
+ *  a "class" object to be worked with/on
+ *  returns object
+ *
+ *  capitalize function name to indicate to other that it is intended to use `new`
+ * use the keyword `this` in constructor
+ *
+ * DO NOT
+ * declare instance explicitly (because `new` does that for us)
+ * run instance explicitly
+ *
+*/
