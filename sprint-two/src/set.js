d2e3c1c54d0aa76834d8096f3bd5d7ee1fdcaff6
David <davidmcruz1125@gmail.com>
2020-08-22 12:39:20 -0500

    Implement a set

--- sprint-two/src/set.js
+++ sprint-two/src/set.js
@@ -1,18 +1,41 @@
 var Set = function() {
   var set = Object.create(setPrototype);
-  set._storage = null; // fix me
+  set._storage = {}; // fix me
   return set;
 };
-
+/**
+ * let set1 = Set();
+ *
+ * set1 = { // this === set1
+ *  _storage = {};
+ *
+ *
+ *
+ *
+ * }
+ */
 var setPrototype = {};
 
 setPrototype.add = function(item) {
+  // check if item !exists in `this` set
+  // (true) => add item to set if
+  if ( !(item in this._storage) ) {
+    this._storage[item] = true;
+    this[item] = true;
+  }
+
 };
 
 setPrototype.contains = function(item) {
+  if ( (item in this._storage) ) {
+    return true;
+  }
+  return false;
 };
 
 setPrototype.remove = function(item) {
+  delete this[item];
+  delete this._storage[item];
 };
 
 /*
